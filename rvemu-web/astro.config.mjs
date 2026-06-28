076c7288e47f950e31a561e9051ed8ab30621b6e
FuseTim <fusetim.log@gmx.com>
2026-06-28 17:55:48 +0200

    ci: deploy to github pages

--- rvemu-web/astro.config.mjs
+++ rvemu-web/astro.config.mjs
@@ -3,6 +3,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+    site: 'https://fusetim.github.io',
+    base: '/rvemu',
     server: {
         headers: {
             'Cross-Origin-Embedder-Policy': 'require-corp',
